95e55c99db81297f2547ab53b1148cc17c23efe1
Antonio Rodriguez <antoniwan@gmail.com>
2020-04-14 18:01:29 -0400

    👌 IMPROVE: add environment var for the port

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
 import "dotenv/config";
+import express from "express";
 
 console.log("Hello node.js project!");
 console.log(process.env.MY_SECRET);
+
+const API_PORT = process.env.API_PORT || 3000;
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send(`Hello World!`);
+});
+
+app.listen(API_PORT, () => {
+  console.log(`Example app listening on port ${API_PORT}`);
+});
